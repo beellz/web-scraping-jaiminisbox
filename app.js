30b98b6b05c1d0ed552cd043320771fd98750ba9
belal <belal20795@gmail.com>
2020-03-06 12:42:56 +0530

    basic first step with cmd argv

---
+++ app.js
@@ -0,0 +1,46 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+// const writeStream = fs.createWriteStream
+const writeStream = fs.createWriteStream('post.txt');
+
+const URL = process.argv[2]
+
+
+// Write Headers
+writeStream.write(`link \n`);
+
+
+request(URL, (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+
+        // const siteHeading = $('.title');
+        // const element = $('.element');
+        // console.log(siteHeading.html());
+        // const output = siteHeading.children('h1').text();
+        // const output = element.children('a').attr('href').html();
+        // console.log(output);
+
+        $('.element').each((i, el) => {
+            const link = $(el)
+            .find ('a')
+            .attr('href');
+
+            console.log(link);
+            writeStream.write(`${link} \n`);
+
+        });
+        console.log("scraping done");
+    }
+});
+
+// fs.writeFile('output.txt', JSON.stringify(link, null , 4), function(error){
+//     if(error) {
+//         console.log(error);
+
+//     }else {
+//         console.log("data inputed");
+
+//     }
+// });
